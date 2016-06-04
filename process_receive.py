06520f9fd2e7e4c543ea1f6398709862728aace1
OstlerDev <ostlerdev@me.com>
2016-06-04 00:50:34 -0600

    Update receive to use MySQL instead of SQLite

--- process_receive.py
+++ process_receive.py
@@ -2,19 +2,19 @@
 """Processes the "receive" table and logs to "action" table. Checks x number of confirmations on receive."""
 
 from flask import Flask
-import sqlite3
+import mysql.connector
 import sys
 from authproxy import AuthServiceProxy, JSONRPCException
 import json
 import requests
 import os
+import decimal
 
 app = Flask(__name__)
 app.config.from_envvar('TRADE_API_SETTINGS')
 
-con = sqlite3.connect(app.config['DATABASE'])
-con.row_factory = sqlite3.Row
-cur = con.cursor()
+cnx = mysql.connector.connect(user=app.config['MYSQL_USER'], password=app.config['MYSQL_PASS'], host=app.config['MYSQL_HOST'], database=app.config['MYSQL_DB'])
+cursor = cnx.cursor()
 
 rpc_user = app.config['RPC_USER']
 rpc_password = app.config['RPC_PASSWORD']
@@ -58,33 +58,20 @@ def get_poloniex_values():
 
     return float(poloniexLastBTCFLO), float(poloniexVolumeFLO)
 
-
-def get_cryptsy_values():
-    url = 'http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=3'
-    try:
-        r = requests.get(url)
-    except requests.exceptions.ConnectionError:
-        print "Unable to connect to Cryptsy API ;_;"
-    m = r.json()['return']['markets']['LTC']
-    cryptsyLastBTCLTC = m['lasttradeprice']
-
-    url = 'http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=61'
-    try:
-        r = requests.get(url)
-    except requests.exceptions.ConnectionError:
-        print "Unable to connect to Cryptsy API ;_;"
-    m = r.json()['return']['markets']['FLO']
-    cryptsyLastLTCFLO = m['lasttradeprice']
-    cryptsyVolumeFLO = m['volume']
-
-    return float(cryptsyLastBTCLTC), float(cryptsyLastLTCFLO), float(cryptsyVolumeFLO)
-
 def process_receive(receive):
+    print receive[1]
+    # Recieve Array Values
+    # [0]: id
+    # [1]: currencyA
+    # [2]: addressA
+    # [3]: amount
+    # [4]: confirmations
+    # [5]: txid
+    # [6]: blockhash
+    # [7]: processed
+
     # The core of how we process receiving of a payment
     # Process the receive and then update Processed
-
-    cryptsyLastBTCLTC, cryptsyLastLTCFLO, cryptsyVolumeFLO  = get_cryptsy_values()
-    print "cryptsy last BTC/LTC: %.8f, last: %.8f, volume: %.2f" % (cryptsyLastBTCLTC, cryptsyLastLTCFLO, cryptsyVolumeFLO)
 
     bittrexLastBTCFLO, bittrexVolumeFLO  = get_bittrex_values()
 
@@ -92,48 +79,66 @@ def process_receive(receive):
     poloniexLastBTCFLO, poloniexVolumeFLO  = get_poloniex_values()
     print "poloniex last: %.8f, volume: %.2f" % (poloniexLastBTCFLO, poloniexVolumeFLO)
 
-    totalVolumeFLO = cryptsyVolumeFLO + bittrexVolumeFLO + poloniexVolumeFLO
+    totalVolumeFLO = bittrexVolumeFLO + poloniexVolumeFLO
 
-    cryptsyWeight = cryptsyLastBTCLTC * cryptsyLastLTCFLO * cryptsyVolumeFLO / totalVolumeFLO
     bittrexWeight =  bittrexLastBTCFLO * bittrexVolumeFLO / totalVolumeFLO
     poloniexWeight = poloniexLastBTCFLO * poloniexVolumeFLO / totalVolumeFLO
-    weightedPrice = cryptsyWeight + bittrexWeight + poloniexWeight
+    weightedPrice = bittrexWeight + poloniexWeight
 
     print "weighted price: %.8f" % weightedPrice
 
     # Gather receiving address
-    cur.execute("SELECT * FROM sendreceivemap WHERE addressA = ?", (receive['addressA'],))
-    result = cur.fetchone()
-    addressB = result["addressB"]
+    # receive[2] = addressA from Table
+    cursor.execute("SELECT * FROM sendreceivemap WHERE addressA = %s;", (receive[2],))
+    result = cursor.fetchone()
+    # sendrecievemap Array Values
+    # [0]: id
+    # [1]: currencyA
+    # [2]: addressA
+    # [3]: currencyB
+    # [4]: addressB
+    addressB = result[4]
+    print addressB
 
     # If the transaction is greater than the 0 conf amount and there have not been enough confirmations then return.
-    if amount > zeroconf and receive['confirmations'] < confirms:
+    # Recieve Array Values
+    # [3]: amount
+    # [4]: confirmations
+    amount = receive[3]
+    confirmations = receive[4]
+    if amount > zeroconf and confirmations < confirms:
         return
 
     # Perform the send
-    currencyBAmount = receive['amount'] / weightedPrice
+    # Recieve Array Values
+    # [3]: amount
+    currencyBAmount = float(receive[3]) / weightedPrice
     txidsend = access.sendtoaddress(addressB, currencyBAmount)
 
     # Log completion
     status = "SENT"
     action = "SENT %.8f FLO TO %s ADDRESS AT BTC RATE %.8f" % (currencyBAmount, addressB, weightedPrice)
-    cur.execute("INSERT INTO action (txidreceive, txidsend, status, action) VALUES (?, ?, ?, ?);"
-        , (receive['txid'], txidsend, status, action))
-    con.commit()
+    # Recieve Array Values
+    # [5]: txid
+    cursor.execute("INSERT INTO action (txidreceive, txidsend, status, action) VALUES (%s, %s, %s, %s);", (receive[5], txidsend, status, action))
+    cnx.commit()
 
     # Update the record once it has been Processed
-    cur.execute("UPDATE receive SET processed = 1 WHERE txid = ?;", (receive['txid'],))
-    con.commit()
+    print receive[1]
+    # Recieve Array Values
+    # [5]: txid
+    cursor.execute("UPDATE receive SET processed = 1 WHERE txid = %s;", (receive[5],))
+    cnx.commit()
 
 # First run a select to see if a receive has been Processed. Exit if it has been Processed.
-with con:
-    # Load all transactions that have not been processed
-    cur.execute("SELECT * FROM receive WHERE processed = 0;")
-    results = cur.fetchall()
 
-if results:
-    for receive in results:
+# Load all transactions that have not been processed
+cursor.execute("SELECT * FROM receive WHERE processed = 0;")
+
+if cursor:
+    for receive in cursor:
         process_receive(receive)
 
-con.close()
+cursor.close()
+cnx.close()
 
